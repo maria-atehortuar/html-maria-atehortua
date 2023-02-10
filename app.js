9b01ff25c57b5b1a7a330140db54e5da97e0359c
maria-atehortuar <123768483+maria-atehortuar@users.noreply.github.com>
2023-02-10 14:00:35 -0500

    apps 1

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {});
+ const cardAdj = [
+ {
+   name: "1.png",
+   img: "images/1.png"
+   },
+
+  ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+  });
