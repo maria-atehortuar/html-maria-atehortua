aa58c852dd786dc0a82ac89748ef3dcc34500993
maria-atehortuar <123768483+maria-atehortuar@users.noreply.github.com>
2023-02-13 12:41:42 -0500

    apps 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {});
- const cardAdj = [
+ condst cardAdj = [
  {
    name: "1.png",
    img: "images/1.png"
@@ -7,9 +7,39 @@ document.addEventListener("DOMContentLoaded", () => {});
 
   ];
 
+//----------------- lecture_02 ----------------------------------//
+{name: "c", img: "images/1.png"},  
+{name: "css", img: "images/2.png"}, 
+{name: "html", img: "images/3.png"}, 
+{name: "c", img: "images/4.png"}, 
+{name: "css", img: "images/5.png"},
+{name: "html", img: "images/6.png"},
+
   const cuadricula = document.querySelector(".cuadricula");
   const resultado = document.querySelector("#resultado");
   var cartasEscogidas = [];
   var cartasEscogidasId = [];
   var cartasGanadas = [];
   });
+
+  //----------------- lecture_03 ----------------------------------// 
+
+  function crearTablero (){
+    for (let i = 0; i < cardAdj.length; i++) { // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
+                                               // cuando i=cardAdj.length, es decir, 12.
+      var carta = document.createElement('img'); // Crea elemento img cada vez que se ejecuta la función.
+      carta.setAttribute('src', 'images/reverso.png'); //Asignar nuevo atributo a cada carta
+                                                       //igual a la imagen 'reverso'.
+      carta.setAttribute('data-id', i); //Asignar como atributo a cada carta creada, el id=i.
+      carta.addEventListener('click', voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta
+                                                     //la función voltearCarta.
+      cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
+                                     // cuadricula de html para que se imprima la carta creada en el div de html.
+
+    }
+
+  }
+
+});
+
+
